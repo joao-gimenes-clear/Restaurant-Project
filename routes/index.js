c8a614064e0a639c6ead67bcf0b89fb58d3e9b45
Joao Gimenes Clear <joao.gimenes.clear@gmail.com>
2023-03-07 11:23:53 -0300

    Feat: Organizating Header with includes

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,8 @@ router.get('/contacts', function(req, res, next){
 
   res.render('contacts', {
     title: 'Contato - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
   })
 
 })
@@ -34,6 +36,8 @@ router.get('/menus', function(req, res, next){
 
   res.render('menus', {
     title: 'Menus - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
   })
 
 })
@@ -42,6 +46,8 @@ router.get('/reservations', function(req, res, next){
 
   res.render('reservations', {
     title: 'Reservas - Restaurante Saboroso!',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
   })
 
 })
@@ -50,6 +56,8 @@ router.get('/services', function(req, res, next){
 
   res.render('services', {
     title: 'Serviços - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
   })
 
 })
