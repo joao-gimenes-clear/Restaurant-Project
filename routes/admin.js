8c90377074558f6d79fe0de04a3e5d5924094140
Joao Gimenes Clear <joao.gimenes.clear@gmail.com>
2023-03-10 08:31:33 -0300

    Feat: CREATE reservation management

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var users = require('./../inc/users');
 var admin = require("./../inc/admin");
-var menus = require("./../inc/menus")
+var menus = require("./../inc/menus");
+var reservations = require("./../inc/reservations");
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -131,6 +132,34 @@ router.get("/reservations", function(req, res, next){
     res.render("admin/reservations", admin.getParams(req, {
         date: {}
     }))
+})
+
+router.post("/reservations", function(req, res, next){
+
+    reservations.save(req.fields, req.files).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
+
+    })
+
+})
+
+router.delete("/reservations/:id", function(req, res, next){
+
+    reservations.delete(req.params.id).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err)
+
+    })
+
 })
 
 router.get("/users", function(req, res, next){
